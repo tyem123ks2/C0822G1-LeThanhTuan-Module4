dd424755d8ac8acd2ab1fd3c74377c8b8a2f36d7
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-02-03 17:07:18 +0700

    first commit

---
+++ ss4_view_thymeleaf/exercise/ProductManagement/src/main/java/com/example/model/Product.java
@@ -0,0 +1,60 @@
+package com.example.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String note;
+    private String producer;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, double price, String note, String producer) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.note = note;
+        this.producer = producer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+}
